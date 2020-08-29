af6ea5bcc6bdb506fbeefaadc857ac8a78e01eec
Jens Reidel <jens@troet.org>
2020-08-29 20:15:26 +0200

    not working but better

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
-use async_std::task::spawn;
-use async_tungstenite::async_std::connect_async;
-use futures::StreamExt;
-pub mod models;
+mod models;
+mod websocket;
 
 #[async_std::main]
 async fn main() {
-    let (ws_stream, _) = connect_async(models::GATEWAY_URL)
-        .await
-        .expect("Failed to connect to the Discord gateway");
-
-    let (_write, read) = ws_stream.split();
-
-    let ws_to_stdout = {
-        read.for_each(|message| async {
-            let message = message.unwrap().into_data();
-            let decoded: models::GatewayPayload =
-                serde_json::from_slice(&message).expect("Cannot decode JSON");
-            println!("{:?}", decoded);
-        })
-    };
-
-    spawn(ws_to_stdout).await;
+    let mut can_connect = true;
+    while can_connect {
+        println!("Connecting!");
+        let mut ws = websocket::WebsocketClient::connect().await;
+        can_connect = ws.read_task().await;
+    }
+    println!("Cannot reconnect");
 }
--- src/models.rs
+++ src/models.rs
@@ -9,16 +9,36 @@ pub const GATEWAY_URL: &str = "wss://gateway.discord.gg/?v=7&encoding=json";
 // https://discord.com/developers/docs/topics/gateway#payloads-gateway-payload-structure
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GatewayPayload {
-    op: OpCode,
-    d: Option<Value>,
-    s: Option<i32>,
-    t: Option<String>,
+    pub op: OpCode,
+    #[serde(rename = "d")]
+    pub data: Option<Value>,
+    #[serde(rename = "s")]
+    pub sequence: Option<i32>,
+    #[serde(rename = "t")]
+    pub event_type: Option<String>,
+}
+
+// https://discord.com/developers/docs/topics/opcodes-and-status-codes#voice-voice-opcodes
+#[derive(Deserialize_repr, Serialize_repr, Debug)]
+#[repr(u8)]
+enum VoiceOpCode {
+    Identify = 0,
+    SelectProtocol = 1,
+    Ready = 2,
+    Heartbeat = 3,
+    SessionDescription = 4,
+    Speaking = 5,
+    HeartbeatAck = 6,
+    Resume = 7,
+    Hello = 8,
+    Resumed = 9,
+    ClientDisconnect = 13,
 }
 
 // https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-opcodes
-#[derive(Deserialize_repr, Serialize_repr, Debug)]
+#[derive(Deserialize_repr, Serialize_repr, PartialEq, Debug)]
 #[repr(u8)]
-enum OpCode {
+pub enum OpCode {
     Dispatch = 0,
     Heartbeat = 1,
     Identify = 2,
@@ -50,4 +70,21 @@ enum CloseCode {
     InvalidApiVersion = 4012,
     InvalidIntents = 4013,
     DisallowedIntents = 4014,
+}
+
+// https://discord.com/developers/docs/topics/opcodes-and-status-codes#voice-voice-close-event-codes
+#[derive(Deserialize_repr, Serialize_repr)]
+#[repr(u16)]
+enum VoiceCloseCode {
+    UnknownOpcode = 4001,
+    NotAuthenticated = 4003,
+    AuthenticationFailed = 4004,
+    AlreadyAuthenticated = 4005,
+    SessionNoLongerValid = 4006,
+    SessionTimeout = 4009,
+    ServerNotFound = 4011,
+    UnknownProtocol = 4012,
+    Disconnected = 4014,
+    VoiceServerCrashed = 4015,
+    UnknownEncryptionMode = 4016,
 }
