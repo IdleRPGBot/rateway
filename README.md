0d72b33de873d3dd2ba9325ec4e4eba193b70e2e
Jens Reidel <jens@troet.org>
2020-08-30 15:26:33 +0200

    move to twilight entirely and scale properly; only AMQP missing

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 A stateless gateway for Discord Bots, especially with Travitia in mind.
 
 Scaling well, it processes messages from the Discord gateway and puts them in AMQP exchanges.
+
+## Configuration
+
+Configuration is entirely done in enviroment variables.
+
+`DISCORD_TOKEN` (**required**): Bot Token
+
+`INTENTS` (**required**): Intents value ([from here](https://ziad87.net/intents/))
+
+`AMQP_URI` (**required**): AMQP URI to push events to
+
+`SHARDS_PER_CLUSTER` (optional, defaults to 8): Shards per cluster
+
+`EXTRA_SHARDS` (optional, defaults to 8): Extra shards to spawn for reserve
+
+## Scalability and Performance
+
+rateway utilizes [simd-json](https://github.com/simd-lite/simd-json) and [cloudflare-zlib](https://gitlab.com/kornelski/cloudflare-zlib-sys) for extremely fast gateway parsing. It supports gateway intents and queues connections over the gateway and its HTTP calls properly, making it practically impossible to hit ratelimits.
+
+rateway puts shards into clusters and spawns a new task for each. Tasks are accelerated in threads across all CPU cores based on their load, using the work-stealing concept. This makes it highly efficient and very scalable, across high and low core count CPUs alike.
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
+use simd_json::to_string;
 use twilight_gateway::cluster::{Cluster, ShardScheme};
-use twilight_model::gateway::GatewayIntents;
+use twilight_gateway::queue::{LargeBotQueue, Queue};
+use twilight_http::Client;
+use twilight_model::gateway::{event::DispatchEvent, GatewayIntents};
+
+use log::info;
+use tokio::task::{spawn, JoinHandle};
 
 use futures::StreamExt;
+use std::convert::{TryFrom, TryInto};
+use std::iter::Iterator;
 use std::sync::Arc;
 use std::{env, error::Error};
 
-mod queue;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    // List of intents to enable
-    // TODO: Move to config
-    let intents = Some(GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES);
-
-    // This is also the default.
-    let scheme = ShardScheme::Auto;
-
-    let cluster = Cluster::builder(env::var("DISCORD_TOKEN")?)
+async fn worker(
+    token: &str,
+    http_client: Client,
+    queue: Arc<Box<dyn Queue>>,
+    intents: Option<GatewayIntents>,
+    scheme: ShardScheme,
+    _cluster_id: usize,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let cluster = Cluster::builder(token)
         .shard_scheme(scheme)
         .intents(intents)
-        .queue(Arc::new(Box::new(queue::RedisQueue)))
+        .http_client(http_client)
+        .queue(queue)
         .build()
         .await?;
 
@@ -31,8 +37,102 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         cluster_spawn.up().await;
     });
 
-    while let Some((id, event)) = events.next().await {
-        println!("Shard: {}, Event: {:?}", id, event.kind());
+    while let Some((_shard_id, event)) = events.next().await {
+        if let Ok(dispatch_evt) = DispatchEvent::try_from(event) {
+            let kind = dispatch_evt.kind();
+            let mut kind_string = to_string(&kind)?;
+            kind_string.remove(0);
+            kind_string.remove(kind_string.len() - 1);
+            // TODO: Filter unwanted events
+            let _serialized = to_string(&dispatch_evt)?;
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    // Read config from env
+    let token = std::env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not set");
+    let intent_value: u64 = std::env::var("INTENTS")
+        .expect("INTENTS not set")
+        .parse()
+        .expect("Cannot parse intents");
+    let shards_per_cluster: usize = std::env::var("SHARDS_PER_CLUSTER")
+        .unwrap_or_else(|_| String::from("8"))
+        .parse()
+        .expect("Cannot parse shards per cluster");
+    let additional_shards: u64 = std::env::var("EXTRA_SHARDS")
+        .unwrap_or_else(|_| String::from("8"))
+        .parse()
+        .expect("Cannot parse extra shards");
+
+    // Set up a HTTPClient
+    let client = Client::new(token.clone());
+
+    // Check total shards required
+    let gateway = client
+        .gateway()
+        .authed()
+        .await
+        .expect("Cannot fetch shard information");
+
+    let total_shards = gateway.shards + additional_shards;
+
+    let intents = Some(GatewayIntents::from_bits_truncate(intent_value));
+
+    // Set up a queue for syncing the auth
+    // This uses the max_concurrency from the gateway automatically
+    let queue: Arc<Box<dyn Queue>> = Arc::new(Box::new(
+        LargeBotQueue::new(
+            gateway
+                .session_start_limit
+                .max_concurrency
+                .try_into()
+                .unwrap(),
+            &client,
+        )
+        .await,
+    ));
+
+    info!("Starting up rateway with {} shards", total_shards);
+    info!("Intents: {:?}", intents);
+
+    let mut all_handles: Vec<JoinHandle<()>> = Vec::new();
+
+    // Spawn a thread for each cluster
+    // Tokio will use a new thread due to rt-threaded
+    for (idx, shard_range) in (0..total_shards)
+        .collect::<Vec<u64>>()
+        .chunks(shards_per_cluster)
+        .enumerate()
+    {
+        let shard_start = shard_range[0];
+        let shard_end = shard_range[shard_range.len() - 1];
+        let new_queue = queue.clone();
+        let new_client = client.clone();
+        let new_token = token.clone();
+        let scheme = ShardScheme::try_from((shard_start..=shard_end, total_shards))?;
+        let handle = spawn(async move {
+            worker(&new_token, new_client, new_queue, intents, scheme, idx + 1)
+                .await
+                .unwrap();
+        });
+        all_handles.push(handle);
+        info!(
+            "Spawned cluster #{} (shards {}-{})",
+            idx + 1,
+            shard_start,
+            shard_end
+        );
+    }
+
+    for handle in all_handles {
+        handle.await.expect("Task failed");
     }
 
     Ok(())
